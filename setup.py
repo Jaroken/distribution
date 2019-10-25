ee0797d954ab2bf117f8ba80d211c6d4645b04fe
Kenneth Preston <kenneth.j.preston@gmail.com>
2019-10-25 13:04:47 +0000

    initialize

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='distribution',
+      version='0.1',
+      description='Distribution package',
+      url='https://github.com/Jaroken/distribution',
+      author='kp',
+      author_email='kenneth.j.preston@gmail.com',
+      license='MIT',
+      zip_safe=False)
